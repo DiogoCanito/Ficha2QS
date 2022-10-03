d8c22ce03520124c061574e7f5fa9c5ac2790bf6
Diogo <Diogo@10.180.4.18>
2022-10-03 14:46:53 +0100

    Versão1 - Atributos e Paramentros

---
+++ Ficha2_Guithub/src/Ficha/UTENTE.java
@@ -0,0 +1,20 @@
+package Ficha;
+
+public class UTENTE 
+{
+	private String nome;
+	private String genero;
+	private int idade;
+	private float altura;
+	private int peso;
+	
+	
+	public UTENTE(String nome, String genero, int idade, float altura, int peso) 
+	{
+		this.nome = nome;
+		this.genero = genero;
+		this.idade = idade;
+		this.altura = altura;
+		this.peso = peso;
+	}
+}
